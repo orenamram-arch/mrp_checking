4ac2ec50ce5e100bf87db5c933a20b62d4f79ec1
orenamram-arch <oren.amram@gmail.com>
2026-08-05 18:53:40 +0300

    Update apy.py

--- apy.py
+++ apy.py
@@ -716,13 +716,13 @@ def check_password():
             st.session_state["password_correct"] = False
 
     if "password_correct" not in st.session_state:
-        st.markdown("### 🔐 כניסה למערכת מגדל בקרת חוסרים (MRP)")
+        st.markdown("### Designed By Oren Amram and Yossi Amar🔐 כניסה למערכת  בקרת חוסרים (MRP)")
         st.text_input("שם משתמש", key="username")
         st.text_input("סיסמה", type="password", key="password")
         st.button("התחבר", on_click=password_entered)
         return False
     elif not st.session_state["password_correct"]:
-        st.markdown("### 🔐 כניסה למערכת מגדל בקרת חוסרים (MRP)")
+        st.markdown("### Designed By Oren Amram and Yossi Amar🔐 כניסה למערכת  בקרת חוסרים (MRP)")
         st.text_input("שם משתמש", key="username")
         st.text_input("סיסמה", type="password", key="password")
         st.button("התחבר", on_click=password_entered)
